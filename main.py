aef3e59dd4568e99f4ee995d84e6680b5f13bf53
anh.nh@platinumtechnology.vn <anh.nh@platinumtechnology.vn>
2025-08-22 21:17:10 +0700

    update1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello, VS Code! Hihi")
+from fastapi import FastAPI # type: ignore
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello FastAPI"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
